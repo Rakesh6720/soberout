b81ef24dc55bf54f3c14a9efb3b6032b1b5f5086
Rakesh6720 <rbaruah@gmail.com>
2022-02-25 09:36:42 -0600

    add navigation component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import NavBarComponent from "../components/layout/nav-bar-component";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <NavBarComponent>
+      <Component {...pageProps} />
+    </NavBarComponent>
+  );
 }
 
 export default MyApp;
