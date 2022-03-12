21db50e06e20fcbac5f0b698d3d36d92f81c5f95
Rakesh Baruah <rbaruah@gmail.com>
2022-03-12 17:11:19 -0600

    add readable date to event item card component

--- components/events/event-item-card-component.js
+++ components/events/event-item-card-component.js
@@ -2,12 +2,18 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import classes from "./event-item-card-component.module.css";
 export default function EventItemCardComponent({ event }) {
+  const readableDate = new Date(event.date).toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <>
       <div className={classes.container}>
         <div className={classes.row}>
           <div className={classes.column}>
-            <h4>{event ? event.date : "loading"}</h4>
+            <h4>{event ? readableDate : "loading"}</h4>
             <h2>{event.title}</h2>
             <h4>{event.description}</h4>
           </div>
